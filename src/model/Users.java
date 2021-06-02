184445149e193eb8a66a2250b418f00343cf3e00
Laura Rubio <laurarubio11@hotmail.com>
2021-06-02 15:30:03 -0500

    init proyect

---
+++ src/model/Users.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Users {
+	
+	private String name;
+	
+	private boolean isAdmin;
+	
+	private String password;
+	
+	private String lastLogin;
+	
+	
+	public Users(String name, boolean isAdmin, String password, String lastLogin) {
+		super();
+		this.name = name;
+		this.isAdmin = isAdmin;
+		this.password = password;
+		this.lastLogin = lastLogin;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLastLogin() {
+		return lastLogin;
+	}
+	public void setLastLogin(String lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+	
+
+}
